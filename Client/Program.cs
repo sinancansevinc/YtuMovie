df261f13ebb36007e97695b02d928e1a7802a0ad
sinancan.sevinc <sinancan_sevinc@hotmail.com>
2021-11-13 19:53:48 +0300

    Client Giriş Yapıldı

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,7 @@
+using Blazored.LocalStorage;
 using Client;
+using Client.Utilities;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<HttpClient>();
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped<AppAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(provider=>provider.GetRequiredService<AppAuthenticationStateProvider>() );
+
 
 await builder.Build().RunAsync();
---
+++ Client/Utilities/AppAuthenticationStateProvider.cs
@@ -0,0 +1,79 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Client.Utilities
+{
+    public class AppAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorageService;
+        private readonly JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+
+        public AppAuthenticationStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+        }
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                string savedToken = await localStorageService.GetItemAsync<string>("bearerToken");
+
+                if (string.IsNullOrEmpty(savedToken))
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+                JwtSecurityToken jwtSecurityToken = jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+                DateTime expiryDate = jwtSecurityToken.ValidTo;
+
+                if (expiryDate < DateTime.UtcNow)
+                {
+                    await localStorageService.RemoveItemAsync("bearerToken");
+                    return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+
+                }
+
+                var claims = ParseClaims(jwtSecurityToken);
+                var user = new ClaimsPrincipal(new ClaimsIdentity(claims,"jwt"));
+
+                return new AuthenticationState(user);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+            }
+        }
+
+        private IList<Claim> ParseClaims(JwtSecurityToken jwtSecurityToken)
+        {
+            
+            IList<Claim> claims=jwtSecurityToken.Claims.ToList();
+
+            claims.Add(new Claim(ClaimTypes.Name, jwtSecurityToken.Subject));
+
+            return claims;
+        }
+
+        internal async Task SignIn()
+        {
+            string savedToken = await localStorageService.GetItemAsync<string>("bearerToken");
+            JwtSecurityToken jwtSecurityToken=jwtSecurityTokenHandler.ReadJwtToken(savedToken);
+            var claims = ParseClaims(jwtSecurityToken);
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwt"));
+
+            Task<AuthenticationState> authenticationState=Task.FromResult(new AuthenticationState(user));
+            NotifyAuthenticationStateChanged(authenticationState);
+
+        }
+        internal void SignOut()
+        {
+            ClaimsPrincipal nobody=new ClaimsPrincipal(new ClaimsIdentity());
+            Task<AuthenticationState> authenticationState = Task.FromResult(new AuthenticationState(nobody));
+            NotifyAuthenticationStateChanged(authenticationState);
+        }
+    }
+}
