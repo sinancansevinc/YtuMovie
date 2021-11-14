bbb5624f72f917877a48641ecf68c4d5ae675151
sinancan.sevinc <sinancan_sevinc@hotmail.com>
2021-11-14 11:26:59 +0300

    Login Page continue

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,15 @@
+﻿namespace Client.Static
+{
+    public static class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "https://localhost:7021";
+
+#else
+        internal const string ServerBaseUrl = "https://appservice.azurewebsites.net";
+#endif
+        internal readonly static string s_register = $"{ServerBaseUrl}/api/user/register";
+        internal readonly static string s_signin = $"{ServerBaseUrl}/api/user/signin";
+
+    }
+}
